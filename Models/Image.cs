6437a45324cb28a9b83fd31231434a355c9491e4
Devin Freeman <freemand@my.lanecc.edu>
2023-04-04 14:00:47 -0700

    --wip-- [skip ci]

--- Models/Image.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HipAndClavicle.Models;
-
-public class Image
-{
-    public int ImageId { get; set; }
-    public byte[] ImageData { get; set; } = default!;
-    public int Width { get; set; } = default!;
-    public int? Height { get; set; } = default!;
-}
