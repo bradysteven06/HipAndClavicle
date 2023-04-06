af1b1c455c67a7dbe87f8338e4da23359f2525c0
Steven Brady <bradys@my.lanecc.edu>
2023-04-06 13:41:35 -0700

    added methods to controller for login view

--- HipAndClavicle/Controllers/AccountController.cs
+++ HipAndClavicle/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HipAndClavicle.Controllers
 {
@@ -9,9 +10,34 @@ namespace HipAndClavicle.Controllers
             return View();
         }
 
-        public IActionResult Login()
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "")
         {
-            return View();
+            LoginVM lvm = new() { ReturnUrl = returnUrl };
+            return View(lvm);
         }
+
+        /*[AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM lvm)
+        {
+
+            var result = await _signInManager.PasswordSignInAsync(lvm.UserName, lvm.Password, isPersistent: lvm.RememberMe, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                _toast.Success("Successfully Logged in as " + lvm.UserName);
+                if (!string.IsNullOrEmpty(lvm.ReturnUrl) && Url.IsLocalUrl(lvm.ReturnUrl))
+                { return Redirect(lvm.ReturnUrl); }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            _toast.Error("Unable to Sign in\n" + result.ToString());
+
+            ModelState.AddModelError("", "Invalid username/password.");
+            return View(lvm);
+        }*/
     }
 }
