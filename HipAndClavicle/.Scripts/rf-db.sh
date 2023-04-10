7fdf42f3bcbbfb3505bb5775ce1668eabc0a5ff6
Devin <freemand@my.lanecc.edu>
2023-04-09 17:08:15 -0700

    --wip-- [skip ci]

--- HipAndClavicle/.Scripts/rf-db.sh
+++ HipAndClavicle/.Scripts/rf-db.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-=======
-export alias refresh="sh .Scripts/rf-db.sh"
-
->>>>>>> f6b757a49d1eddb176cd701cc052bdbe39ddf702
 current=$(pwd)
 
 cd $current
@@ -13,25 +8,14 @@ cd $current
 read -p "Enter a name for the new migration: " migration_name
 
 # Drop the database 
-<<<<<<< HEAD
 dotnet ef database drop -f --no-build
 
 # Remove the last migration 
 
-dotnet ef migrations remove --no-build
+dotnet ef migrations remove
 
 # Add a new migration with the user input as the name 
-dotnet ef migrations add $migration_name
-=======
-dotnet ef database drop -f 
-
-# Remove the last migration 
-
-dotnet ef migrations remove 
-
-# Add a new migration with the user input as the name 
-dotnet ef migrations add $migration_name 
->>>>>>> f6b757a49d1eddb176cd701cc052bdbe39ddf702
+dotnet ef migrations add $migration_name --no-build
 
 # Update the database with the new migration 
 dotnet ef database update
