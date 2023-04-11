0df8add85f3888e1972ce452b85f08cdbbffa68e
Devin Freeman <102371929+Dfreem@users.noreply.github.com>
2023-04-11 15:35:20 -0700

    Revert "switching computers (#12)" (#13)
    This reverts commit ead1a8bea87ba98b6f7c54f5f805ddbd6fae448e.

--- HipAndClavicle/Controllers/CustomerProductCatalogController.cs
+++ HipAndClavicle/Controllers/CustomerProductCatalogController.cs
@@ -4,13 +4,6 @@ namespace HipAndClavicle.Controllers
 {
     public class CustomerProductCatalogController : Controller
     {
-        private readonly IHipRepo _repo;
-        private readonly ApplicationDbContext _context;
-        public CustomerProductCatalogController(IHipRepo repo, ApplicationDbContext context)
-        {
-            _repo = repo;
-            _context = context;
-        }
         public IActionResult Index()
         {
             return View();
@@ -19,19 +12,6 @@ namespace HipAndClavicle.Controllers
         public IActionResult SearchByColor() 
         {
             return View();
-        }
-
-        public async Task<IActionResult> ViewAllProducts()
-        {
-            var products = await _context.Products.ToListAsync();
-            var listings = await _context.Listings.ToListAsync();
-            ProductListingVM productsVM = new ProductListingVM();
-            productsVM.Products = products;
-            productsVM.Listings = listings;
-            
-
-
-            return View(productsVM);
         }
     }
 }
--- HipAndClavicle/ViewModels/ProductListingVM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HipAndClavicle.ViewModels
-{
-    public class ProductListingVM
-    {
-        public List<Product> Products { get; set; } = new List<Product>();
-        public List<Listing> Listings { get; set; } = new List<Listing>();
-    }
-}
