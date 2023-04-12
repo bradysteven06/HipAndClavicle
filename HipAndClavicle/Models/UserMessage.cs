96cf85de197cbaeb821681b96c6ecfa67b3438b0
Devin Freeman <freemand@my.lanecc.edu>
2023-04-11 20:25:27 -0700

    we figured out gitub yeah

--- HipAndClavicle/Models/UserMessage.cs
+++ HipAndClavicle/Models/UserMessage.cs
@@ -2,10 +2,11 @@
 
 public class UserMessage
 {
+    public int UserMessageId { get; set; }
     [Required]
     public AppUser Sender { get; set; } = default!;
     public DateTime DateSent { get; } = DateTime.Now;
-    public bool Read { get; set; } = false;
+    public bool Read { get; set; }
     public string? Response { get; set; }
     public int? OrderId { get; set; }
 }
