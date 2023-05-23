13933b669151a93115506e98433c1e5c4221af5e
Steven <63345588+bradysteven06@users.noreply.github.com>
2023-05-23 12:19:06 -0700

    Unit testing (#87)
    * added HIPNunitTests Project to the solution
    
    * added Nunit tests project to solution
    
    * added empty classes for product tests and shopping cart tests

---
+++ HIPNunitTests/ProductTests.cs
@@ -0,0 +1,17 @@
+namespace HIPNunitTests
+{
+    public class ProductTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ HIPNunitTests/ShoppingCartTests.cs
@@ -0,0 +1,16 @@
+﻿
+namespace HIPNunitTests
+{
+    public class ShoppingCartTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
